85d27e5b197c033244c8a2fb31c17273c2c81300
kushalvala28296 <kushalvala28296@gmail.com>
2023-11-02 23:22:22 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import Nav from "./components/Nav";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+
+const App = () => {
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x padding-t bg-black pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
+};
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,27 @@
+export const Button = ({
+  label,
+  backgroundColor,
+  textColor,
+  borderColor,
+  iconURL,
+  fullWidth,
+}) => {
+  return (
+    <button
+      className={` flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${
+        backgroundColor
+          ? ` ${backgroundColor} ${textColor} ${borderColor}`
+          : `bg-coral-red  text-white border-coral-red`
+      } ${fullWidth && "w-full"} `}
+    >
+      {label}
+      {iconURL && (
+        <img
+          src={iconURL}
+          className="ml-2 rounded-full w-5 h-5"
+          alt="right-icon"
+        />
+      )}
+    </button>
+  );
+};
